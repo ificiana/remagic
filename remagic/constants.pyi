e639b10281f6a44e15f56e6ff19143221096ec04
ificiana <ificiana@gmail.com>
2022-09-08 05:17:51 +0530

    add negated constants

--- remagic/constants.pyi
+++ remagic/constants.pyi
@@ -4,12 +4,19 @@ class Consts:
 
     CHAR: str
     WHITESPACE: str
+    NOT_WHITESPACE: str
     WS: str
+    NOT_WS: str
     WORD: str
+    NOT_WORD: str
     DIGIT: str
+    NOT_DIGIT: str
     LETTER: str
+    NOT_LETTER: str
     TAB: str
-    LINEFEED: str
+    NEWLINE: str
+    NOT_NEWLINE: str
     CARRIAGE_RETURN: str
     N: str
+    NOT_N: str
     R: str
