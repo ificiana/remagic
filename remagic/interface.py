c7c12b5053cf52ed2a228d38ecb43b49019b3e8b
ificiana <ificiana@gmail.com>
2022-09-08 03:30:24 +0530

    add new functions to the interface

--- remagic/interface.py
+++ remagic/interface.py
@@ -1,5 +1,6 @@
 from .constants import Consts
 from .pattern import Pattern
+from .utils import char_set_escape, escape
 
 
 def create(pattern):
@@ -24,3 +25,18 @@ def zero_or_more(pattern, greedy=True):
 
 def one_or_more(pattern, greedy=True):
     return Pattern(pattern) * (1,) if greedy else optional(Pattern(pattern) * (1,))
+
+
+def char_in(iterable):
+    f = [char_set_escape(i) for i in iterable]
+    return exactly(f"[{''.join(f)}]")
+
+
+def char_not_in(iterable):
+    f = [char_set_escape(i) for i in iterable]
+    return exactly(f"[^{''.join(f)}]")
+
+
+def any_of(iterable):
+    f = [escape(i) for i in iterable]
+    return exactly(f"[{'|'.join(f)}]")
