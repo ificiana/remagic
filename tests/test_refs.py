2613530378243c3dd6426b3d70814a2af7865802
ificiana <ificiana@gmail.com>
2022-10-29 20:10:09 +0530

    write new tests

--- tests/test_refs.py
+++ tests/test_refs.py
@@ -3,6 +3,7 @@
 import pytest
 
 from remagic import *
+from remagic.exceptions import RemagicException
 
 
 @pytest.mark.parametrize(
@@ -17,6 +18,17 @@ def test_refs(capture, name, expected_output):
 
 
 @pytest.mark.parametrize(
+    "capture, name, expected_output",
+    [
+        ("captured", "text", "(?P<text>captured)(?P=text)"),
+    ],
+)
+def test_refs_create(capture, name, expected_output):
+    output = create(capture).label("text") + ref("text")
+    assert output == expected_output, f"{output} != {expected_output}"
+
+
+@pytest.mark.parametrize(
     "capture, expected_output",
     [
         ("captured", r"(captured)\1"),
@@ -25,3 +37,49 @@ def test_refs(capture, name, expected_output):
 def test_groups(capture, expected_output):
     output = create(capture).group().ref(1)
     assert output == expected_output, f"{output} != {expected_output}"
+
+
+@pytest.mark.parametrize(
+    "capture, expected_output",
+    [
+        ("captured", r"(captured)\1"),
+    ],
+)
+def test_groups_create(capture, expected_output):
+    output = create(capture).group() + ref(1)
+    assert output == expected_output, f"{output} != {expected_output}"
+
+
+@pytest.mark.parametrize(
+    "capture, expected_output",
+    [
+        ("captured", r"(captured)\1"),
+    ],
+)
+def test_groups_create(capture, expected_output):
+    output = create(capture).group() + ref(1)
+    assert output == expected_output, f"{output} != {expected_output}"
+
+
+@pytest.mark.parametrize(
+    "label, expected_output",
+    [
+        (["list"], pytest.raises(RemagicException)),
+    ],
+)
+def test_ref_error(label, expected_output):
+    with expected_output:
+        output = ref(label)
+        assert output == expected_output, f"{output} != {expected_output}"
+
+
+@pytest.mark.parametrize(
+    "label, expected_output",
+    [
+        (["list"], pytest.raises(RemagicException)),
+    ],
+)
+def test_ref_error2(label, expected_output):
+    with expected_output:
+        output = create("test").ref(label)
+        assert output == expected_output, f"{output} != {expected_output}"
