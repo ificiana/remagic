7e549640fce7d93a50525c64c2fedf5ae476e2f4
Nilendu Ghoshal <ghoshalsubhranil@gmail.com>
2022-10-30 14:00:46 +0530

    Removed duplicate test

--- tests/test_refs.py
+++ tests/test_refs.py
@@ -51,17 +51,6 @@ def test_groups_create(capture, expected_output):
 
 
 @pytest.mark.parametrize(
-    "capture, expected_output",
-    [
-        ("captured", r"(captured)\1"),
-    ],
-)
-def test_groups_create(capture, expected_output):
-    output = create(capture).group() + ref(1)
-    assert output == expected_output, f"{output} != {expected_output}"
-
-
-@pytest.mark.parametrize(
     "label, expected_output",
     [
         (["list"], pytest.raises(RemagicException)),
